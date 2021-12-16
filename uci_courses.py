2cc7a3ca6670ee8c987b66e8a992e2bd9f045a04
Tram La <latb@uci.edu>
2021-12-15 16:54:37 -0800

    re-scrape uci_courses. Added description, restriction, and prereq

--- uci_courses.py
+++ uci_courses.py
@@ -3,7 +3,7 @@ import requests
 from major_requirements import request_websites, scrape_courses
 from collections import namedtuple
 
-Course_Info = namedtuple('Course', ['name', 'units'])
+Course_Info = namedtuple('Course', ['name', 'units', 'description'])
 
 def get_courses_websites():
 
@@ -19,16 +19,21 @@ def get_courses(url):
     course_dict = {}
     soup = request_websites(url)
     for elem in soup.find_all('div', class_='courseblock'):
+        description = '' 
         name = elem.find('p', class_='courseblocktitle').text.split('.  ')
-        c_info = Course_Info(name[1], (name[2].split(' ')[0]))
+        testing = elem.find('div', class_='courseblockdesc')
+        get_info = testing.find_all('p')
+        for each in get_info:
+            description += each.text + ';'
+        c_info = Course_Info(name[1], (name[2].split(' ')[0]), description)
         course_dict[name[0]] = c_info
     return course_dict
 
 def write_to_csv(url, one_course):
     filename = str(url.split('/')[-2])
-    with open(filename + '.csv', 'w') as f:
+    with open('data/'+filename + '.out', 'w') as f:
         for key, value in one_course.items():
-            f.write(key + ',' + value.name + ',' + value.units + '\n')
+            f.write(key + ';' + value.name + ';' + value.units + ';' + value.description + '\n')
 
 if __name__ == "__main__":
     websites = get_courses_websites()
