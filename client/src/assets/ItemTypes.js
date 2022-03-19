cb9e2d712a80cab19a4366f846fa103c6fde2744
Loc Linh <lockhong6395@gmail.com>
2022-03-19 01:58:03 -0700

    remove folder

--- client/src/assets/ItemTypes.js
+++
@@ -1,3 +0,0 @@
-const ItemTypes = "COURSE"
-export default ItemTypes;
- 
--- client/src/pages/AboutPage.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function AboutPage() {
-    return (
-        <div>AboutPage</div>
-    );
-}
-
-export default AboutPage;
