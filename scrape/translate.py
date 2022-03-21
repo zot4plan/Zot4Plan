17553fb61532d15e4ba730c8dd7d751ef0719671
Tram La <latb@uci.edu>
2022-03-21 02:42:35 -0700

    Adjust data styling

--- scrape/translate.py
+++ scrape/translate.py
@@ -1,6 +1,6 @@
 import json 
 
-file_names = ["../data/Data_Science_B.S.json"]
+file_names = ["../data/Software_Engineering_B.S.json"]
 for elem in file_names: 
     with open(elem, 'r') as f: 
         data = json.load(f)
