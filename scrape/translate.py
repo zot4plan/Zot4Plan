f79405682c104176166c4a2b0232a97c511de15f
Tram La <latb@uci.edu>
2022-03-23 18:46:46 -0700

    Edit Major Requirements

--- scrape/translate.py
+++ scrape/translate.py
@@ -1,6 +1,6 @@
 import json 
 
-file_names = ["../data/Film_and_Media_Studies_B.A.json"]
+file_names = ["../data/holder.json"]
 for elem in file_names: 
     with open(elem, 'r') as f: 
         data = json.load(f)
