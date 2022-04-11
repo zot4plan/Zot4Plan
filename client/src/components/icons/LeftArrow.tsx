79b9a86f26d7c8dea81b673381e4231704bb9024
Loc Linh <lockhong6395@gmail.com>
2022-04-10 21:37:05 -0700

    fix tutorial image size

--- client/src/components/icons/LeftArrow.tsx
+++ client/src/components/icons/LeftArrow.tsx
@@ -1,6 +1,6 @@
 function LeftArrow () {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="10rem" height="10rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path fill="currentColor" d="m14 5l-5 5l5 5l-1 2l-7-7l7-7z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="8rem" height="8rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path fill="currentColor" d="m14 5l-5 5l5 5l-1 2l-7-7l7-7z"/></svg>
     )
 }
 
--- client/src/components/tutorial/TutorialSlider.tsx
+++ client/src/components/tutorial/TutorialSlider.tsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import LeftArrow from '../icons/LeftArrow'
 import RightArrow from '../icons/RightArrow'
 import XCircle from '../icons/XCircle'
-import Right from '../icons/RightArrow'
-import HowTo1 from '../../images/HowTo1.png'
 
 interface TutorialSliderType{
     closedTutorial:any;
 
 }
-
 
 function TutorialSlider ({ closedTutorial }: TutorialSliderType) {
 
@@ -33,12 +30,16 @@ function TutorialSlider ({ closedTutorial }: TutorialSliderType) {
     
       console.log(imagesSrc[index]);
       return (
-        <div id='tutorial' style={ {display: 'flex'} }>
+        <div id='tutorial' className='flex justify-center item-center'>
             <button className='arrow' onClick={handleLeftClick}>
                 <LeftArrow/>
             </button>
-            <div className='relative'>
-                <div className='absolute' id='x-circle' onClick={closedTutorial}>
+            <div id='tutorial-image-container' 
+                 className='flex justify-center relative'>
+                <div 
+                    className='absolute' 
+                    id='x-circle' 
+                    onClick={closedTutorial}>
                     <XCircle/>
                 </div>
                 <img
