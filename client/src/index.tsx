a2c24197a8b0d0d9d7ea65346c8953cea10d2276
Loc Linh <lockhong6395@gmail.com>
2022-05-01 14:57:54 -0700

    update package-lock, added margin-top to footer

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
@@ -14,7 +13,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
