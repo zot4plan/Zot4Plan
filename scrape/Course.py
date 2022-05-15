792f87ab56611c8f5435169211df5a31f64066b2
Tram La <latb@uci.edu>
2022-05-14 22:28:13 -0700

    Update scraping method for courses (add Course class)

---
+++ scrape/Course.py
@@ -0,0 +1,104 @@
+"""
+Course is a class is responsible for storing information of each individual
+UCI courses. 
+"""
+class Course:
+    """
+    Initialize all attributes needed to store a UCI course's information
+    """
+    def __init__(self):
+        self.name = ''
+        self.course_key = ''
+        self.department = ''
+        self.units = '0'
+        self.description = ''
+        self.prerequisite = ''
+        self.corequisite = ''
+        self.repeatability = '1'
+        self.restriction = ''
+        self.ge_string = ''
+        self.ge_list = []
+
+
+    def set_header_info(self, header_info):
+        """
+        set_header takes in a raw text (Course header in UCI catalogue) 
+        and split the string up by parts ('.'). Using given
+        informtation, set attributes: name, course_key, and untis
+        :param header_info: string that contains course key, course name, and units 
+        """
+
+        header_info = header_info.text.split('.  ')
+        self.name = header_info[1]
+        self.course_key = header_info[0].replace("\u00a0", " ")
+
+        get_dept = self.course_key.split(" ")[:-1]
+        self.department = " ".join(get_dept)
+
+        if len(header_info[2].split(' ')[0]) > 1:
+            self.units = header_info[2].split(' ')[0][-1]
+        elif len(header_info[2].split(' ')[0]) == 1:
+            self.units = header_info[2].split(' ')[0]
+
+
+    def set_description(self, raw_description):
+        """
+        set_description takes in a raw text was scraped from UCI Catalogue and
+        set the value to attribute description
+        :param raw_description: string that contains the course description
+        """
+        self.description = raw_description.replace('"', "'")
+
+
+    def set_information(self, raw_info):
+        """
+        set_information takes in a raw text was scraped from UCI Catalogue and set the
+        value based on its content. The class method searches for certain key terms and
+        execute accordingly. Possible class attribute changes: restriction, prerequisite,
+        and corequisite
+        :param raw_info: a string containing course information
+        """
+
+        raw_info = raw_info.replace('"', "'")
+        
+        if 'Restriction:' in raw_info:
+            self.restriction = raw_info.replace('Restriction:', '')
+        elif 'Prerequisite:' in raw_info:
+            if 'Corequisite:' in raw_info:
+                separate = raw_info.split('\n')
+                self.prerequisite = separate[1].replace('Prerequisite:','').replace('\xa0ENG\xa0', ' ')
+                self.corequisite = separate[0].replace('Corequisite:','')
+            else:
+                self.prerequisite = raw_info.replace('\n', '').replace('"', "'").replace('Prerequisite:','').replace('\xa0ENG\xa0', ' ')
+        elif self.corequisite != '' and 'Corequisite:' in raw_info:
+            self.corequisite = raw_info.replace('\n', '').replace('Corequisite:','')
+    
+    
+    def set_ge(self, raw_text_ge=None):
+        """
+        set_ge takes in a raw text that contains information regarding course's GE category. The string
+        will be saved in two format: string (display purposes) and list (checking purposes).
+        :param raw_text_ge: string that contains information regarding course's GE
+        """
+
+        if raw_text_ge is not None:
+            self.ge_string = raw_text_ge.text.replace('.', '').upper()
+            in_list = self.ge_string.replace(',', '').replace(')', '').replace('(', '').replace(')', '').replace('AND', '').replace('OR', '').replace('GE', '').split(' ')
+            self.ge_list = [elem.strip() for elem in in_list if elem != '']
+
+    
+    def set_repeatability(self, raw_info):
+        """
+        set_repeatability takes in a raw text scraped from the UCI course catalogue and
+        determines how many time a course can be taken for credit
+        :param raw_info: a string that contains course's information regarding course repeatability
+        """
+
+        if 'Unlimited' in raw_info or 'unlimited' in raw_info:
+            self.repeatability = '9'
+        else:
+            repeat = raw_info.split(' ')
+            for char in repeat:
+                if char.isdigit():
+                    self.repeatability = char
+                    break
--- scrape/scrape_courses.py
+++ scrape/scrape_courses.py
@@ -1,9 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 from major_requirements import request_websites, scrape_courses
-from collections import namedtuple
-
-Course_Info = namedtuple('Course', ['name', 'department', 'units', 'description','prerequisite', 'corequisite', 'repeatability', 'restriction', 'ge_string', 'ge_list'])
+from Course import Course
+import json
 
 def get_courses_websites():
     """
@@ -26,56 +25,60 @@ def get_courses(url):
     """
     get_courses takes in each invidiual website and scrape all courses provided in the url
     and their information (course id, course name, units, description, restriction, and prereq).
-    All the information is organized in a dictionary: id: namedtuple(info).
+    All the information is organized in a dictionary: id: Course Object
     """
+    
     course_dict = {}
     soup = request_websites(url)
     for elem in soup.find_all('div', class_='courseblock'):
-        name = elem.find('p', class_='courseblocktitle').text.split('.  ')
-        testing = elem.find('div', class_='courseblockdesc')
-        get_info = testing.find_all('p')
-        ge_tag, restrict, prereq, description, repeat, co_course = '', '', '', '', '1', ''
-        ge_list = []
+        header = elem.find('p', class_='courseblocktitle')
+        section = elem.find('div', class_='courseblockdesc')
+        get_info = section.find_all('p')
+        course_info = Course()
+        
+        course_info.set_header_info(header)
+
         for x in range(len(get_info)):
             info = get_info[x].text
             ge = get_info[x].find('strong')
             if x == 0:
-                description = info.replace('"', "'")
-            elif 'Restriction:' in info:
-                restrict = info.replace('"', "'").replace('Restriction:', '')
-            elif 'Prerequisite:' in info:
-                prereq = info.replace('\n', '').replace('"', "'").replace('Prerequisite:','').replace('\xa0ENG\xa0', ' ')
-            elif 'Corequisite:' in info:
-                co_course = info.replace('\n', '').replace('"', "'").replace('Corequisite:','')
-            elif 'Repeatability:' in info:
-                if 'Unlimited' in info or 'unlimited' in info:
-                    repeat = '9'
-                else:
-                    repeat = info.split(' ')
-                    for elem in repeat:
-                        if elem.isdigit():
-                            repeat = elem
-            if ge is not None:
-                ge_tag = ge.text.replace('.', '').upper()
-                in_list = ge_tag.replace(',', '').replace(')', '').replace('(', '').replace(')', '').replace('AND', '').replace('OR', '').replace('GE', '').split(' ')
-                ge_list = [elem.strip() for elem in in_list if elem != '']
-        unit = "0"
-        if len(name[2].split(' ')[0]) > 1:
-            unit = name[2].split(' ')[0][-1]
-        elif len(name[2].split(' ')[0]) == 1:
-            unit = name[2].split(' ')[0]
+                course_info.set_description(info)
+            elif 'Repeatability:' in info and 'Restriction:' not in info:
+                course_info.set_repeatability(info)
+            else:
+                course_info.set_information(info)
+
+            course_info.set_ge(ge)
         
-        key_name = name[0].replace("\u00a0", " ")
-        get_dept = key_name.split(" ")[:-1]
-        department = " ".join(get_dept)
-        c_info = Course_Info(name=name[1], department=department, units=unit, description=description, prerequisite=prereq, corequisite=co_course,
-                                repeatability=repeat ,restriction=restrict, ge_string=ge_tag, ge_list=ge_list)
-        course_dict[key_name] = c_info
+        course_dict[course_info.course_key] = course_info
     return course_dict
 
 
-if __name__ == "__main__":
+def write_courses():
+    """
+    write_courses calls get_courses_websites to collect all current UCI courses from UCI General Catalogue.
+    Using those collected websites, get_courses will be called to scrape all of the courses offered at UCI by departments.
+    All information regarding courses will be saved in uci_courses.sql.
+    All information regarding GE courses will be saved in alll_GEs.sql.
+    """
+
     websites = get_courses_websites()
-    for each_url in websites:
-        one_course = get_courses(each_url)
-        ##print(one_course)
+    course_names = []
+    write_ge = open('../database/all_GEs.sql', 'w')
+
+    with open('../database/uci_courses.sql', 'w') as f:
+        for each_url in websites:
+            uci_course = get_courses(each_url)
+            for key,value in uci_course.items():
+                course_names.append(key)
+                f.write('INSERT INTO courses VALUES ("' + key + '","' + value.name.strip() +  '","' + value.department + '","' + value.units + '","' + value.description + '","' + value.prerequisite + '","' + value.restriction + '","' + value.repeatability + '","' + value.corequisite + '","' + value.ge_string + '");' + '\n')
+                for cat in value.ge_list:
+                    write_ge.write('INSERT INTO courses_in_ges (courseId, geId) VALUES ("' + key + '","' + cat + '");' + '\n')
+    write_ge.close()
+
+    with open('../data/data.json', 'w') as f: 
+        json.dump(course_names, f,indent=4)
+
+
+if __name__ == "__main__":
+    write_courses()
